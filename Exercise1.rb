091bfec3e4c7ca4a42d908029112f75c8eb222bb
James Quon <joquon@gmail.com>
2012-01-30 23:19:21 -0700

    numbers_1-1000

--- Exercise1.rb
+++ Exercise1.rb
@@ -4,8 +4,18 @@ class Exercise1
 		@today = Time.now
 	end
 	
-	def getCurrDate
+	def get_curr_date
 		puts @today.to_s
+	end
+	
+	def get_big_array
+		# @big_array = []
+		@big_array = Array.new 
+		# Fill array with the numbers 1-1000
+		for i in 1..1000
+			@big_array.push(i)
+		end	
+		puts @big_array
 	end
 	
 end
